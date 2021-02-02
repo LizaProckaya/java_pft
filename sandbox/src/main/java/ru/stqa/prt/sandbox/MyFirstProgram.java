3d0f506e8201de98c0441f615bf0f8bce33ef889
LizaProckaya <78159675+LizaProckaya@users.noreply.github.com>
2021-02-03 01:12:05 +0300

    Выполнение пункта 1,2,3 из Задания2

--- sandbox/src/main/java/ru/stqa/prt/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/prt/sandbox/MyFirstProgram.java
@@ -3,18 +3,13 @@ package ru.stqa.prt.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    hello("world");
-    hello("Lisa");
-
-    Square s = new Square(5);
-    System.out.println("Площадь квадрата со стороной" + s.l + " = " + s.area());
-
-    Rectangle r = new Rectangle(4,6);
-    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+    Point p1 = new Point(1.0, 2.0);
+    Point p2 = new Point(3.8, 4.9);
+    System.out.println("Расстояние между точками " + "x1 = " + p1.x + "," + "y1 = " + p1.y + " и " + "x2 = " + p2.x + "," + "y2 = " + p2.y + " = " + distance(p1, p2));
   }
 
-  public static void hello(String somebody ){
-    System.out.println("Hello," + somebody + "!");
+  public static double distance(Point p1, Point p2) {
+    double d = Math.sqrt(Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2));
+    return d;
   }
-
 }
