a05aa9b8da9914492ba2a4734abb9cd9771eff45
LizaProckaya <78159675+LizaProckaya@users.noreply.github.com>
2021-02-03 01:51:15 +0300

    Финальный вариант Задания2 (4 пункт)

--- sandbox/src/main/java/ru/stqa/prt/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/prt/sandbox/MyFirstProgram.java
@@ -5,11 +5,7 @@ public class MyFirstProgram {
   public static void main(String[] args) {
     Point p1 = new Point(1.0, 2.0);
     Point p2 = new Point(3.8, 4.9);
-    System.out.println("Расстояние между точками " + "x1 = " + p1.x + "," + "y1 = " + p1.y + " и " + "x2 = " + p2.x + "," + "y2 = " + p2.y + " = " + distance(p1, p2));
+    System.out.println("Расстояние между точками " + "x1 = " + p1.x + "," + "y1 = " + p1.y + " и " + "x2 = " + p2.x + "," + "y2 = " + p2.y + " = " + p1.distance(p2));
   }
 
-  public static double distance(Point p1, Point p2) {
-    double d = Math.sqrt(Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2));
-    return d;
-  }
 }
--- sandbox/src/main/java/ru/stqa/prt/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/prt/sandbox/Point.java
@@ -3,9 +3,15 @@ package ru.stqa.prt.sandbox;
 public class Point {
   public double x;
   public double y;
-  public Point(double x, double y){
+
+  public Point(double x, double y) {
     this.x = x;
     this.y = y;
 
+  }
+
+  public double distance(Point p) {
+    double d = Math.sqrt(Math.pow(p.x - x, 2) + Math.pow(p.y - y, 2));
+    return d;
   }
 }
