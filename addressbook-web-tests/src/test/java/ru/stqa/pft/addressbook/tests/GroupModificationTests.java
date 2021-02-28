57e49d6d59d34ea294b2f8f16928029ac8a0c7db
LizaProckaya <78159675+LizaProckaya@users.noreply.github.com>
2021-03-01 00:40:10 +0300

    модификация метода Select для выбора элемента списка по порядковому номеру

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -12,13 +12,13 @@ public class GroupModificationTests extends TestBase {
     if (!app.getGroupHelper().isThereAGroup()) {
       app.getGroupHelper().createGroup(new GroupData("test1", null, null));
     }
-    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().selectGroup(before - 1);
     app.getGroupHelper().initGroupModification();
     app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
     app.getGroupHelper().submitGroupModification();
     app.getGroupHelper().returnToGroupPage();
     int after = app.getGroupHelper().getGroupCount();
-    Assert.assertEquals(after,before);
+    Assert.assertEquals(after, before);
     app.getSessionHelper().logout();
   }
 }
