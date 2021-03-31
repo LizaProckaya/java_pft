2c700353e61d87a74332721ba3fcc95301aece20
LizaProckaya <78159675+LizaProckaya@users.noreply.github.com>
2021-03-31 22:37:01 +0300

    Автоматическое закрытие файлов после использования

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/Tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/Tests/ContactCreationTests.java
@@ -23,39 +23,41 @@ public class ContactCreationTests extends TestBase {
 
   @DataProvider
   public Iterator<Object[]> validContactsFromXml() throws IOException {
-    BufferedReader reader = new BufferedReader(new FileReader(new File(("src/test/resources/contacts.xml"))));
-    String xml = "";
-    String line = reader.readLine();
-    while (line != null) {
-      xml += line;
-      //String[] split = line.split(";");
-      //list.add(new Object[] {new ContactData().withFirstName(split[0]).withMiddleName(split[1]).withLastName(split[2]).withNickname(split[3]).withGroupName(split[4])});
-      line = reader.readLine();
+    try (BufferedReader reader = new BufferedReader(new FileReader(new File(("src/test/resources/contacts.xml"))))){
+      String xml = "";
+      String line = reader.readLine();
+      while (line != null) {
+        xml += line;
+        //String[] split = line.split(";");
+        //list.add(new Object[] {new ContactData().withFirstName(split[0]).withMiddleName(split[1]).withLastName(split[2]).withNickname(split[3]).withGroupName(split[4])});
+        line = reader.readLine();
+      }
+      XStream xstream = new XStream();
+      xstream.processAnnotations(ContactData.class);
+      List<ContactData> contacts = (List<ContactData>) xstream.fromXML(xml);
+      return contacts.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
     }
-    XStream xstream = new XStream();
-    xstream.processAnnotations(ContactData.class);
-    List<ContactData> contacts = (List<ContactData>) xstream.fromXML(xml);
-    return contacts.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
   }
   @DataProvider
   public Iterator<Object[]> validContactsFromJson() throws IOException {
-    BufferedReader reader = new BufferedReader(new FileReader(new File(("src/test/resources/contacts.json"))));
-    String json = "";
-    String line = reader.readLine();
-    while (line != null) {
-      json += line;
-      // Для csv:
-      //String[] split = line.split(";");
-      //list.add(new Object[] {new ContactData().withFirstName(split[0]).withMiddleName(split[1]).withLastName(split[2]).withNickname(split[3]).withGroupName(split[4])});
-      line = reader.readLine();
+    try (BufferedReader reader = new BufferedReader(new FileReader(new File(("src/test/resources/contacts.json"))))){
+      String json = "";
+      String line = reader.readLine();
+      while (line != null) {
+        json += line;
+        // Для csv:
+        //String[] split = line.split(";");
+        //list.add(new Object[] {new ContactData().withFirstName(split[0]).withMiddleName(split[1]).withLastName(split[2]).withNickname(split[3]).withGroupName(split[4])});
+        line = reader.readLine();
+      }
+      Gson gson =  new Gson();
+      List<ContactData> contacts = gson.fromJson(json,new TypeToken<List<ContactData>>(){}.getType()); //List<GroupData>.class
+      //Для xml:
+      //XStream xstream = new XStream();
+      //xstream.processAnnotations(ContactData.class);
+      //List<ContactData> contacts = (List<ContactData>) xstream.fromXML(json);
+      return contacts.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
     }
-    Gson gson =  new Gson();
-    List<ContactData> contacts = gson.fromJson(json,new TypeToken<List<ContactData>>(){}.getType()); //List<GroupData>.class
-    //Для xml:
-    //XStream xstream = new XStream();
-    //xstream.processAnnotations(ContactData.class);
-    //List<ContactData> contacts = (List<ContactData>) xstream.fromXML(json);
-    return contacts.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
   }
 
   @Test(dataProvider = "validContactsFromJson")
