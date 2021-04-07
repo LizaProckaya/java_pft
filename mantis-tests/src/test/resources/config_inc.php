6b8f542fb36bc732f205cc01d92bbaebf4d2a90d
LizaProckaya <78159675+LizaProckaya@users.noreply.github.com>
2021-04-08 00:03:31 +0300

    Получение почты на встроенный в тесты почтовый сервер

--- mantis-tests/src/test/resources/config_inc.php
+++ mantis-tests/src/test/resources/config_inc.php
@@ -9,4 +9,6 @@ $g_default_timezone       = 'Europe/Berlin';
 
 $g_crypto_master_salt     = 'u0TkexDaxfOsp5xKxdzD1XTnmVd6JXNtw5DQSKu8tUk=';
 
-$g_signup_use_captcha     = OFF; 
+$g_signup_use_captcha     = OFF;
+$g_phpMailer_method       = PHPMAILER_METHOD_SMTP;
+$g_smtp_host              = 'localhost';
