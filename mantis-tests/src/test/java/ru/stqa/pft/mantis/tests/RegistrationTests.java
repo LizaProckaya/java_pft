6bc90d82440b8d28ea98d4653a873dea9e52dca3
LizaProckaya <78159675+LizaProckaya@users.noreply.github.com>
2021-04-08 01:32:59 +0300

    Получение почты на внешний почтовый сервер. Создание пользователей на внешнем почтовом сервере (по протоколу Telnet)

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
@@ -1,7 +1,5 @@
 package ru.stqa.pft.mantis.tests;
 
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.lanwen.verbalregex.VerbalExpression;
 import ru.stqa.pft.mantis.model.MailMessage;
@@ -14,7 +12,7 @@ import static org.testng.AssertJUnit.assertTrue;
 
 public class RegistrationTests extends TestBase{
 
-  @BeforeMethod
+ // @BeforeMethod
   public void startMailServer(){
     app.mail().start();
   }
@@ -22,11 +20,13 @@ public class RegistrationTests extends TestBase{
   @Test
   public void testRegistration()  throws IOException, MessagingException {
     long now = System.currentTimeMillis();
-    String email = String.format("user%s@localhost.localdomain",now);
+    String email = String.format("user%s@localhost",now);
     String user = String.format("user%s", now);
     String password = "password";
+    app.james().createUser(user,password);
     app.registration().start(user, email);
-    List<MailMessage> mailMessages = app.mail().waitForMail(2, 10000);
+    //List<MailMessage> mailMessages = app.mail().waitForMail(2, 10000);
+    List<MailMessage> mailMessages = app.james().waitForMail(user,password,60000);
     String confirmationLink = findConfirmationLink(mailMessages, email);
     app.registration().finish(confirmationLink, password);
     assertTrue(app.newSession().login(user,password));
@@ -38,7 +38,7 @@ public class RegistrationTests extends TestBase{
     return regex.getText(mailMessage.text);
   }
 
-  @AfterMethod (alwaysRun = true)
+ // @AfterMethod (alwaysRun = true)
   public void stopMailServer(){
     app.mail().stop();
   }
