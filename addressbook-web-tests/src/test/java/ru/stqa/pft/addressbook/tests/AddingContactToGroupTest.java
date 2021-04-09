3d82e34720ea3e43f98e5d8b2bb918a57a6fd1e3
LizaProckaya <78159675+LizaProckaya@users.noreply.github.com>
2021-04-09 19:53:24 +0300

    Правки к заданию16

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/AddingContactToGroupTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/AddingContactToGroupTest.java
@@ -4,8 +4,12 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.Contacts;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.Groups;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -23,16 +27,29 @@ public class AddingContactToGroupTest extends TestBase {
 
   @Test
   public void testAdding() {
-    ContactData contactBefore = app.db().contacts().iterator().next();
+    Contacts contacts = app.db().contacts();
+    List<ContactData> contactsList = new ArrayList<>(contacts);
     Groups groups = app.db().groups();
+    ContactData contactBefore = null;
 
-    if (contactBefore.getGroups().size() == groups.size()) {
-      app.goTo().groupPage();
-      app.group().create(new GroupData().withName("testAdding"));
-      app.goTo().contactPage();
-      groups = app.db().groups();
+    for (int i = 0; i < contactsList.size(); i++) {
+      contactBefore = contactsList.get(i);
+      if (contactBefore.getGroups().size() < groups.size()) {
+        break;
+      } else if (contactBefore.getGroups().size() == groups.size() && i == contactsList.size() - 1) {
+        app.goTo().groupPage();
+        app.group().create(new GroupData().withName("testAdding"));
+        app.goTo().contactPage();
+        groups = app.db().groups();
+      }
     }
 
+    Assert.assertNotNull(contactBefore);
+
+    /*
+    Удаляем из всех загруженных групп те, в которых состоит контакт.
+    В итоге, в groups получаем доступные для записи группы. Можем взять оттуда любой, но для простоты берем первый
+     */
     groups.removeAll(contactBefore.getGroups());
 
     GroupData availableGroup = groups.stream().iterator().next();
@@ -41,11 +58,13 @@ public class AddingContactToGroupTest extends TestBase {
 
     app.contact().addContactsToGroupById(availableGroup.getId());
 
-    ContactData contactAfter = app.db().contacts().stream().filter(contactData -> contactData.getId() == contactBefore.getId()).iterator().next();
+    ContactData finalContactBefore = contactBefore;
+    ContactData contactAfter = app.db().contacts().stream().filter(contactData -> contactData.getId() == finalContactBefore.getId()).iterator().next();
 
     Assert.assertNotNull(contactAfter);
     assertThat(contactBefore.getGroups().size() + 1, equalTo(contactAfter.getGroups().size()));
     assertThat((int) contactAfter.getGroups().stream().filter(g -> g.getId() == availableGroup.getId()).count(), equalTo(1));
+    assertThat(contactAfter.getGroups(), equalTo(contactBefore.getGroups().withAdded(availableGroup)));
   }
 
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/DeletingContactFromGroupTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/DeletingContactFromGroupTest.java
@@ -4,9 +4,12 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.Contacts;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.Groups;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Collectors;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -25,35 +28,48 @@ public class DeletingContactFromGroupTest extends TestBase {
 
   @Test
   public void testDeleting() {
-    ContactData contactBefore = app.db().contacts().iterator().next();
+    Contacts contacts = app.db().contacts();
+    List<ContactData> contactsList = new ArrayList<>(contacts);
     Groups groups = app.db().groups();
-    GroupData group;
+    List<GroupData> groupData = new ArrayList<>(groups);
+    ContactData contactBefore = null;
+    GroupData group = null;
 
-    if (contactBefore.getGroups().size() == 0 && groups.size() != 0) {
-      // добавить в группу
-      app.contact().selectContactById(contactBefore.getId());
-      group = groups.stream().iterator().next();
-      app.contact().addContactsToGroupById(group.getId());
-    } else if (groups.size() == 0) {
-      // создать и добавить
-      app.goTo().groupPage();
-      String newGroupName = "testAdding";
-      app.group().create(new GroupData().withName(newGroupName));
-      app.goTo().contactPage();
-      app.contact().selectContactById(contactBefore.getId());
-      app.contact().addContactsToGroupByVisibleText(newGroupName);
-      app.goTo().contactPage();
-      group = app.db().groups().stream().filter(g -> g.getName().equals(newGroupName)).collect(Collectors.toList()).get(0);
-    } else {
-      group = contactBefore.getGroups().stream().iterator().next();
+    for (int i = 0; i < contactsList.size(); i++) {
+      contactBefore = contactsList.get(i);
+      if (contactBefore.getGroups().size() != 0) {
+        group = new ArrayList<>(contactBefore.getGroups()).get(0);
+        break;
+      } else if (contactBefore.getGroups().size() == 0 && i == contactsList.size() - 1 && groups.size() != 0) {
+        // добавить в группу
+        app.contact().selectContactById(contactBefore.getId());
+        group = groupData.get(0);
+        app.contact().addContactsToGroupById(group.getId());
+      } else if (groups.size() == 0) {
+        // создать и добавить
+        app.goTo().groupPage();
+        String newGroupName = "testAdding";
+        app.group().create(new GroupData().withName(newGroupName));
+        app.goTo().contactPage();
+        app.contact().selectContactById(contactBefore.getId());
+        app.contact().addContactsToGroupByVisibleText(newGroupName);
+        app.goTo().contactPage();
+        group = app.db().groups().stream().filter(g -> g.getName().equals(newGroupName)).collect(Collectors.toList()).get(0);
+      }
     }
+
+    Assert.assertNotNull(contactBefore);
+    Assert.assertNotNull(group);
 
     app.contact().removeContactFromGroup(group.getId(), contactBefore.getId());
 
-    ContactData contactAfter = app.db().contacts().stream().filter(c -> c.getId() == contactBefore.getId()).collect(Collectors.toList()).get(0);
+    ContactData finalContactBefore = contactBefore;
+    ContactData contactAfter = app.db().contacts().stream().filter(c -> c.getId() == finalContactBefore.getId()).collect(Collectors.toList()).get(0);
 
     Assert.assertNotNull(contactAfter);
-    assertThat((int) contactAfter.getGroups().stream().filter((g) -> g.getId() == group.getId()).count(), equalTo(0));
+    GroupData finalGroup = group;
+    assertThat((int) contactAfter.getGroups().stream().filter((g) -> g.getId() == finalGroup.getId()).count(), equalTo(0));
+    assertThat(contactAfter.getGroups().withAdded(group), equalTo(contactBefore.getGroups()));
   }
 
 }
