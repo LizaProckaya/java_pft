6271287c4456c9d527c7ac553f344f1fb33cb1f3
LizaProckaya <78159675+LizaProckaya@users.noreply.github.com>
2021-04-09 06:08:14 +0300

    Скриншоты к заданию 21

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -33,6 +33,5 @@ public class GroupModificationTests extends TestBase {
     Groups after = app.db().groups();
     assertThat(after, equalTo(before.without(modifiedGroup).withAdded(group)));
     verifyGroupListInUI();
-    
   }
 }
